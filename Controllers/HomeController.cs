d528c855ac030ced1c6e72c82e270066bf33c923
Andrew <andrewmgunn26@gmail.com>
2022-05-05 08:33:36 -0500

    everything migrated now

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace Identity.Controllers
             userManager = userMgr;
         }
 
+// this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+
+        public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
         /*[Authorize]
         public IActionResult Index()
         {
---
+++ Models/CartItemJSON.cs
@@ -0,0 +1,9 @@
+namespace Identity.Models
+{
+    public class CartItemJSON
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public int qty { get; set; }
+    }
+}
