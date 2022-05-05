ff39b4920a925cf658c8c0a057238e1b1b9e8538
Andrew <andrewmgunn26@gmail.com>
2022-05-05 08:43:08 -0500

    running - but home page is wrpong

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Identity.Controllers
         private NorthwindContext _northwindContext;
         public HomeController(NorthwindContext db) => _northwindContext = db;
 
-        public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
+        //public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
         /*[Authorize]
         public IActionResult Index()
         {
